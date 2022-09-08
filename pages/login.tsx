75b5216780443ecd2336a395533403cc312838ef
wisdomin121@naver.com <wisdomin121@naver.com>
2022-09-08 21:00:00 +0900

    로그인 레이아웃

---
+++ pages/login.tsx
@@ -0,0 +1,3 @@
+import { LoginScreen } from "@/screen";
+
+export default LoginScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        white: "#FFFFFF",
+        gray0: "#DBDBDB",
+        gray1: "#9B9EA1",
+        gray2: "#CCCCCC",
+        gray3: "#8F9295",
+        black: "#383838",
+        red: "#E33A3A",
+        green: "#4CAF50",
         accent1: "#4B99EB",
         accent2: "#1CDDAD",
         accent3: "#E9F4FF",
+        sky0: "#F0F9FF",
       },
     },
   },
