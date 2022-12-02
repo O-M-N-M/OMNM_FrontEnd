3c745aa19e1a657255f6d2fb4863fcd03c0ef5d3
wisdomin121@naver.com <wisdomin121@naver.com>
2022-12-02 15:44:28 +0900

    ✨ [FEAT] : 랜딩페이지 API 연결
    전체 사용자 수 API 연결

--- screen/landing/landing.screen.tsx
+++ screen/landing/landing.screen.tsx
@@ -1,16 +1,33 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Box, Typography, Button } from "@mui/material";
 
-import LogoString from '../../public/logo3.png';
+import LogoString1 from '../../public/logo3.png';
+import LogoString2 from '../../public/logo4.png';
 import LogoBoth from '../../public/logo.png';
-import Link from "next/link";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const LadingScreen: NextPage = () => {
+  const [totalUser, setTotalUser] = useState(0);
+
+  useEffect(() => {
+    const getTotalUser = async () => {
+      const url = '/api/home';
+      await axios.get(url).then((res) => {
+        setTotalUser(res.data.userCount);
+      });
+    }
+
+    getTotalUser();
+  }, [totalUser])
+
   return (
     <>
-      <Box className='flex flex-row mt-40'>
+      <Box className='flex flex-row my-40'>
         <Box className="ml-[15%]">
           <Typography className="text-[3.3rem] font-bold">
             {`오늘 만나고 내일 만나는\n기숙사 `}
@@ -21,7 +38,7 @@ export const LadingScreen: NextPage = () => {
           </Typography>
 
           <Typography className="text-[1.68rem] font-normal mt-3">
-            <Image src={LogoString} width={114.67} height={19.65} />
+            <Image src={LogoString1} width={114.67} height={19.65} />
             {` 에서 나와 잘맞는 룸메를 찾아보세요.`}
           </Typography>
 
@@ -34,6 +51,22 @@ export const LadingScreen: NextPage = () => {
 
         <Box className="ml-auto mr-[15%]">
           <Image src={LogoBoth} width={275} height={280} />
+        </Box>
+      </Box>
+
+      <Box className="flex flex-row">
+        <Box className='my-36 ml-[15%]'>
+          <Typography className="text-black text-2xl">지금까지</Typography>
+
+          <Box className="flex flex-row mt-3">
+            <Image src={LogoString2} width={195.73} height={33.52} />
+            <Typography className="text-accent1 text-4xl font-bold ml-6">누적 가입자 수</Typography>
+            <Typography className="text-black text-4xl font-bold">는?</Typography>
+          </Box>
+        </Box>
+
+        <Box className='flex bg-white w-80 h-36 rounded-full drop-shadow-[0px_4px_10px_rgba(0,0,0,0.14)] justify-center items-center my-36 ml-auto mr-[15%]'>
+          <Typography className="text-black text-5xl font-bold">{totalUser} 명</Typography>
         </Box>
       </Box>
     </>
