d37aa808750cc0fc85fa363df020e860764fb2be
unknown <wjdrkdud5@gmail.com>
2022-12-06 23:36:12 +0900

    add footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,42 +1,50 @@
-import Image from "next/image";
-
 import { Box, Typography } from "@mui/material";
-import logo from '../public/logo.png';
+import Image from "next/image";
 import Link from "next/link";
+
+import logo from "../public/logo.png";
 
 const Footer = () => {
   return (
     <Box className="flex items-center bg-gray5 w-full h-[180px]">
       <Box className="ml-[15%]">
         <Image src={logo} width={60} height={61} />
-        <Typography className="text-gray4 text-xs font-regular mt-1">ⓒ 2022. omnm. All rights reserved.</Typography>
+        <Typography className="text-gray4 text-xs font-regular mt-1">
+          ⓒ 2022. omnm. All rights reserved.
+        </Typography>
       </Box>
 
       <Box className="flex flex-row ml-auto mr-[15%]">
-        <Link href='/main'>
+        <Link href="/main">
           <a>
             <Typography className="font-medium text-xs">메인</Typography>
           </a>
         </Link>
 
         <Box className="ml-14">
-          <Link href='/'>
+          <Link href="/mypage">
             <a>
-              <Typography className="font-medium text-xs mb-2">마이페이지</Typography>
+              <Typography className="font-medium text-xs mb-2">
+                마이페이지
+              </Typography>
             </a>
           </Link>
 
-          <Link href='/'>
+          <Link href="/mypage_receive">
             <a>
-              <Typography className="font-regular text-xs mb-2">룸메 신청 내역</Typography>
+              <Typography className="font-regular text-xs mb-2">
+                룸메 신청 내역
+              </Typography>
             </a>
           </Link>
-          <Link href='/'>
+          <Link href="/mypage_mysurvey">
             <a>
-              <Typography className="font-regular text-xs mb-2">성향 설문조사</Typography>
+              <Typography className="font-regular text-xs mb-2">
+                성향 설문조사
+              </Typography>
             </a>
           </Link>
-          <Link href='/'>
+          <Link href="/">
             <a>
               <Typography className="font-regular text-xs">설정</Typography>
             </a>
@@ -79,6 +87,6 @@ const Footer = () => {
     </Box> */
     // </Box>
   );
-}
+};
 
 export default Footer;
