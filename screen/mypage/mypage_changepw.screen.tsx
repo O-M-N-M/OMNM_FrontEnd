3eed210f407195d590e0902016fb9d3376a3c1e3
wisdomin121@naver.com <wisdomin121@naver.com>
2022-12-17 22:22:09 +0900

    🔨 [MOD] : 마이페이지 푸터 추가
    마이페이지에 있는 페이지들에 푸터를 추가했음

--- screen/mypage/mypage_changepw.screen.tsx
+++ screen/mypage/mypage_changepw.screen.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
+import Footer from "../../components/footer";
 import MyPageLeft from "../../components/mypage/mypage_left";
 
 export const MyPageChangePwScreen = () => {
@@ -13,57 +14,61 @@ export const MyPageChangePwScreen = () => {
   }, [newPw, checkNewPw])
 
   return (
-    <form className='flex flex-row justify-center min-h-[calc(100vh-50px)] my-[5%]'>
-      <Box>
-        <MyPageLeft />
-      </Box>
-
-      <Box className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-fit h-fit px-72 py-20 ml-6'>
-        <Typography className='text-black text-xl font-medium text-center w-full'>비밀번호 변경</Typography>
-
-        <Box className='mt-10'>
-          <Typography className='text-black text-base font-medium'>현재 비밀번호</Typography>
-          <input
-            type="password"
-            name="pw"
-            placeholder="현재 비밀번호 입력"
-            value={nowPw}
-            onChange={(e) => setNowPw(e.target.value)}
-            className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
-            required />
+    <>
+      <Box className='flex flex-row justify-center min-h-[calc(100vh-50px)] my-[5%]'>
+        <Box>
+          <MyPageLeft />
         </Box>
 
-        <Box className='mt-7'>
-          <Box className='flex flex-row items-center'>
-            <Typography className='text-black text-base font-medium'>새 비밀번호</Typography>
-            <Typography className='text-gray1 text-xs font-regular ml-2'>6~12자 이내 숫자, 특수문자, 영문자 모두 포함</Typography>
+        <form className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-fit h-fit px-72 py-20 ml-6'>
+          <Typography className='text-black text-xl font-medium text-center w-full'>비밀번호 변경</Typography>
+
+          <Box className='mt-10'>
+            <Typography className='text-black text-base font-medium'>현재 비밀번호</Typography>
+            <input
+              type="password"
+              name="pw"
+              placeholder="현재 비밀번호 입력"
+              value={nowPw}
+              onChange={(e) => setNowPw(e.target.value)}
+              className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
+              required />
           </Box>
-          <input
-            type="password"
-            name="pw"
-            placeholder="새 비밀번호 입력"
-            value={newPw}
-            onChange={(e) => setNewPw(e.target.value)}
-            className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
-            required />
-        </Box>
 
-        <Box className='mt-7'>
-          <Typography className='text-black text-base font-medium'>새 비밀번호 확인</Typography>
-          <input
-            type="password"
-            name="pw"
-            placeholder="새 비밀번호 재입력"
-            value={checkNewPw}
-            onChange={(e) => setCheckNewPw(e.target.value)}
-            className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
-            required />
-        </Box>
+          <Box className='mt-7'>
+            <Box className='flex flex-row items-center'>
+              <Typography className='text-black text-base font-medium'>새 비밀번호</Typography>
+              <Typography className='text-gray1 text-xs font-regular ml-2'>6~12자 이내 숫자, 특수문자, 영문자 모두 포함</Typography>
+            </Box>
+            <input
+              type="password"
+              name="pw"
+              placeholder="새 비밀번호 입력"
+              value={newPw}
+              onChange={(e) => setNewPw(e.target.value)}
+              className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
+              required />
+          </Box>
 
-        <Button className='bg-accent1 rounded-full mt-9 px-8 py-2.5 ml-auto'>
-          <Typography className='text-white text-sm font-medium'>확인</Typography>
-        </Button>
+          <Box className='mt-7'>
+            <Typography className='text-black text-base font-medium'>새 비밀번호 확인</Typography>
+            <input
+              type="password"
+              name="pw"
+              placeholder="새 비밀번호 재입력"
+              value={checkNewPw}
+              onChange={(e) => setCheckNewPw(e.target.value)}
+              className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
+              required />
+          </Box>
+
+          <Button className='bg-accent1 rounded-full mt-9 px-8 py-2.5 ml-auto'>
+            <Typography className='text-white text-sm font-medium'>확인</Typography>
+          </Button>
+        </form>
       </Box>
-    </form>
+
+      <Footer />
+    </>
   );
 }
--- screen/mypage/mypage_edit.screen.tsx
+++ screen/mypage/mypage_edit.screen.tsx
@@ -9,6 +9,7 @@ import { PhotoCamera } from "@mui/icons-material";
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
+import Footer from "../../components/footer";
 import MyPageLeft from '../../components/mypage/mypage_left'
 import basicProfile from '../../public/basicProfile.png';
 
@@ -72,70 +73,73 @@ export const MyPageEditScreen = () => {
   }, []);
 
   return (
-    <Box className='flex flex-row justify-center min-h-[calc(100vh-50px)] my-[5%]'>
-      <Box>
-        <MyPageLeft />
-      </Box>
-
-      <form onSubmit={onSubmit} className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-fit h-fit px-72 py-20 ml-6'>
-        <Typography className='text-black text-xl font-medium text-center w-full'>개인정보 수정</Typography>
-
-        <Box className="relative text-center mt-10">
-          {
-            profile !== null ?
-              <Image src={URL.createObjectURL(profile)} width={90} height={90} className="absolute rounded-full" />
-              :
-              image === null ?
-                <Image src={basicProfile} width={90} height={90} className="absolute rounded-full" />
-                : (
-                  <Box className='border border-gray1 border-solid rounded-full w-[90px] h-[90px]'>
-                    <Image loader={() => image} src={image} width={90} height={90} className="absolute rounded-full" />
-                  </Box>
-                )
-          }
-          <IconButton aria-label="upload picture" component="label" className="absolute top-14 right-[-15px] bg-white border border-solid border-gray1">
-            <input
-              hidden
-              accept="image/*"
-              type="file"
-              onChange={(e) => {
-                if (e.target.files !== null) {
-                  setProfile(e.target.files[0]);
-                }
-              }} />
-            <PhotoCamera fontSize="small" />
-          </IconButton>
-        </Box>
-
+    <>
+      <Box className='flex flex-row justify-center min-h-[calc(100vh-50px)] my-[5%]'>
         <Box>
-          <Typography className='text-black text-base font-medium mt-10'>카카오톡 ID</Typography>
-          <input
-            type="text"
-            name="id"
-            placeholder={kakaoId}
-            value={kakaoId}
-            onChange={(e) => setKakaoId(e.target.value)}
-            className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
-            required />
+          <MyPageLeft />
         </Box>
 
-        <Box className='w-full'>
-          <Typography className='text-black text-base font-medium mt-7'>생활관 정보</Typography>
-          <FormControl>
-            <RadioGroup row onChange={(e) => setDormitory(e.target.value)} className='flex flex-col'>
-              <Box>
-                <FormControlLabel value="0" control={<Radio checked={dormitory === '0'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[0]}</Typography>} />
-                <FormControlLabel value="1" control={<Radio checked={dormitory === '1'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[1]}</Typography>} />
-              </Box>
-              <FormControlLabel value="2" control={<Radio checked={dormitory === '2'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[2]}</Typography>} />
-            </RadioGroup>
-          </FormControl>
-        </Box>
+        <form onSubmit={onSubmit} className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-fit h-fit px-72 py-20 ml-6'>
+          <Typography className='text-black text-xl font-medium text-center w-full'>개인정보 수정</Typography>
 
-        <Button className='bg-accent1 rounded-full mt-6 px-8 py-2.5 ml-auto'>
-          <Typography className='text-white text-sm font-medium'>완료</Typography>
-        </Button>
-      </form>
-    </Box>
+          <Box className="relative text-center mt-10">
+            {
+              profile !== null ?
+                <Image src={URL.createObjectURL(profile)} width={90} height={90} className="absolute rounded-full" />
+                :
+                image === null ?
+                  <Image src={basicProfile} width={90} height={90} className="absolute rounded-full" />
+                  : (
+                    <Box className='border border-gray1 border-solid rounded-full w-[90px] h-[90px]'>
+                      <Image loader={() => image} src={image} width={90} height={90} className="absolute rounded-full" />
+                    </Box>
+                  )
+            }
+            <IconButton aria-label="upload picture" component="label" className="absolute top-14 right-[-15px] bg-white border border-solid border-gray1">
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={(e) => {
+                  if (e.target.files !== null) {
+                    setProfile(e.target.files[0]);
+                  }
+                }} />
+              <PhotoCamera fontSize="small" />
+            </IconButton>
+          </Box>
+
+          <Box>
+            <Typography className='text-black text-base font-medium mt-10'>카카오톡 ID</Typography>
+            <input
+              type="text"
+              name="id"
+              placeholder={kakaoId}
+              value={kakaoId}
+              onChange={(e) => setKakaoId(e.target.value)}
+              className='border border-solid border-gray0 rounded-full text-gray1 text-xs font-regular w-80 p-4 mt-2 focus:outline-none'
+              required />
+          </Box>
+
+          <Box className='w-full'>
+            <Typography className='text-black text-base font-medium mt-7'>생활관 정보</Typography>
+            <FormControl>
+              <RadioGroup row onChange={(e) => setDormitory(e.target.value)} className='flex flex-col'>
+                <Box>
+                  <FormControlLabel value="0" control={<Radio checked={dormitory === '0'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[0]}</Typography>} />
+                  <FormControlLabel value="1" control={<Radio checked={dormitory === '1'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[1]}</Typography>} />
+                </Box>
+                <FormControlLabel value="2" control={<Radio checked={dormitory === '2'} icon={<CheckCircleOutlineIcon sx={{ color: "#DBDBDB" }} />} checkedIcon={<CheckCircleIcon />} size="small" />} label={<Typography className="text-black text-base font-regular">{dormitoryTitle[2]}</Typography>} />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+
+          <Button className='bg-accent1 rounded-full mt-6 px-8 py-2.5 ml-auto'>
+            <Typography className='text-white text-sm font-medium'>완료</Typography>
+          </Button>
+        </form>
+      </Box>
+      <Footer />
+    </>
   )
 }
