2213fcbfff96bc6e91f2b2655f042f6443b5e0e2
wisdomin121@naver.com <wisdomin121@naver.com>
2022-12-28 12:44:49 +0900

    ✨ [FEAT] : 회원탈퇴 통신
    회원탈퇴 통신 완료함

--- screen/mypage/mypage_withdrawal.screen.tsx
+++ screen/mypage/mypage_withdrawal.screen.tsx
@@ -1,12 +1,69 @@
-import { Box, Button, Typography } from "@mui/material"
-import { useState } from "react";
+import { getCookie } from "cookies-next";
+
+import { Box, Button, Typography } from "@mui/material";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import MyPageLeft from '../../components/mypage/mypage_left';
 import Footer from "../../components/footer";
 
 export const MyPageWithdrawalScreen = () => {
+  const [userId, setUserId] = useState('');
+  const [open, setOpen] = useState(false);
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
+
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
+
+  const onSubmit = async () => {
+    const url = `/api/users/${userId}`;
+    const body = `loginId=${id}&password=${pw}`;
+
+    const token = getCookie('OMNM');
+    const headers = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        'OMNM': `${token}`
+      }
+    };
+    const userDto = {
+      loginId: `${id}`,
+      password: `${pw}`
+    };
+
+    const formData = new FormData();
+    formData.append(
+      'key',
+      new Blob([JSON.stringify(userDto)],
+        { type: "application/json" }
+      )
+    )
+
+    await axios.post(url, body, headers)
+      .then(() => document.location = '/landing')
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    const getMyData = async () => {
+      const url = '/api/myInfo';
+      const token = getCookie('OMNM');
+      const headers = {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          'OMNM': `${token}`
+        }
+      };
+
+      await axios.get(url, headers)
+        .then((res) => setUserId(res.data.userId))
+        .catch((err) => console.log(err));
+    }
+
+    getMyData();
+  }, []);
 
   return (
     <>
@@ -15,7 +72,8 @@ export const MyPageWithdrawalScreen = () => {
           <MyPageLeft />
         </Box>
 
-        <form className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-full h-fit py-20 ml-6'>
+        <form onSubmit={onSubmit} className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-full h-fit py-20 ml-6'>
+          {/* <Box className='flex flex-col justify-center items-center border border-solid border-gray0 rounded-[1.25rem] w-full h-fit py-20 ml-6'> */}
           <Typography className='text-black text-xl font-medium text-center w-full'>회원 탈퇴</Typography>
 
           <Box className='mt-10'>
@@ -43,11 +101,28 @@ export const MyPageWithdrawalScreen = () => {
           </Box>
 
           <Box className='flex w-80'>
-            <Button className='bg-accent1 rounded-full mt-9 px-8 py-2.5 ml-auto'>
+            <Button type='submit' className='bg-accent1 rounded-full mt-9 px-8 py-2.5 ml-auto'>
+              {/* <Button onClick={onSubmit} className='bg-accent1 rounded-full mt-9 px-8 py-2.5 ml-auto'> */}
               <Typography className='text-white text-sm font-medium'>확인</Typography>
             </Button>
           </Box>
+
+          {/* {
+            open &&
+            <Modal
+              open={open}
+              onClose={handleClose}
+            >
+              <Box sx={{ position: 'absolute', backgroundColor: 'white', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', borderRadius: '10px', width: '40%', minWidth: '400px', height: 'fit-content', outline: 'none', paddingX: '', paddingY: '3rem' }}>
+                <Typography sx={{ color: '#383838', fontSize: '1.125rem', fontWeight: '400' }}>회원을 탈퇴하시겠습니까?</Typography>
+                <Typography sx={{ color: '#E33A3A', fontSize: '0.875rem', fontWeight: '400' }}>(회원 탈퇴 시, 모든 개인정보가 영구 삭제됩니다.)</Typography>
+                <Button onClick={onSubmit}>예</Button>
+                <Button onClick={handleClose}>아니오</Button>
+              </Box>
+            </Modal>
+          } */}
         </form>
+        {/* </Box> */}
       </Box>
 
       <Footer />
