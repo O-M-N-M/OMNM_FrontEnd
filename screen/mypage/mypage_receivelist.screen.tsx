08455401b7220a459d1ce5264dcdd2872e894173
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-06 00:24:14 +0900

    🔨 [MOD] : 신청 리스트 페이지 레이아웃 완성
    lifeCycle을 제외한 나머지 레이아웃 완성함

--- screen/mypage/mypage_receivelist.screen.tsx
+++ screen/mypage/mypage_receivelist.screen.tsx
@@ -67,12 +67,12 @@ export const MyPageReceiveListScreen = () => {
 
   return (
     <Box>
-      <Box className='flex flex-row justify-center min-h-[calc(100vh-50px)] w-screen my-[5%]'>
+      <Box className='flex flex-row justify-center min-h-[calc(100vh-50px)] mx-[15%] my-[5%]'>
         <Box>
           <MyPageLeft />
         </Box>
 
-        <Box className='border border-solid border-gray0 rounded-[1.25rem] h-fit px-[2.875rem] py-16 ml-6'>
+        <Box className='border border-solid border-gray0 rounded-[1.25rem] w-full h-fit px-[2.875rem] py-16 ml-6'>
           <Box className='flex flex-row items-center'>
             <Typography className='text-black text-xl font-medium'>룸메 신청 받은 리스트</Typography>
             <IconButton className='ml-auto'>
@@ -81,11 +81,11 @@ export const MyPageReceiveListScreen = () => {
           </Box>
 
           <Box className='flex flex-col items-center mt-5'>
-            <Box className='min-h-[44rem]'>
+            <Box className='w-full min-h-[44rem]'>
               {
                 data.map((v: any, index: number) => {
                   return (
-                    <Box key={index} className='flex flex-row items-center border border-solid border-gray0 rounded-xl w-fit h-fit mt-4 px-6 py-3'>
+                    <Box key={index} className='flex flex-row items-center border border-solid border-gray0 rounded-xl w-full h-fit mt-4 px-6 py-3'>
                       <Box className='border border-solid border-gray1 rounded-full px-1 py-0.5'>
                         {
                           v.time[5] === '0' ? (
@@ -107,8 +107,8 @@ export const MyPageReceiveListScreen = () => {
 
                       <Typography className='text-black text-base font-medium ml-3 w-16'>{v.name}</Typography>
                       <Typography className='text-gray1 text-xs font-regular ml-2'>· {v.age}</Typography>
-                      <Typography className='text-black text-xs font-regular ml-5 w-1'>{v.mbti}</Typography>
-                      <Button className='bg-white border border-solid border-accent1 rounded-full ml-32'>
+                      <Typography className='text-black text-xs font-regular ml-5 w-8'>{v.mbti}</Typography>
+                      <Button className='bg-white border border-solid border-accent1 rounded-full ml-auto'>
                         <Typography className='text-accent1 text-xs font-regular'>프로필 보기</Typography>
                       </Button>
                     </Box>
