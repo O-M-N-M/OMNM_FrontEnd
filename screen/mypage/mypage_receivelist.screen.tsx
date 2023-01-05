a5e519d6568ef7cf6665ef891b1de6675e55c8a3
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-05 23:52:14 +0900

    🔨 [MOD] : 메인페이지 배경 gif 파일 변경
    수정 버전으로 변경함

--- screen/mypage/mypage_receivelist.screen.tsx
+++ screen/mypage/mypage_receivelist.screen.tsx
@@ -80,20 +80,34 @@ export const MyPageReceiveListScreen = () => {
             </IconButton>
           </Box>
 
-          <Box className='flex flex-col items-center mt-9'>
+          <Box className='flex flex-col items-center mt-5'>
             <Box className='min-h-[44rem]'>
               {
                 data.map((v: any, index: number) => {
                   return (
-                    <Box key={index} className='flex flex-row items-center border border-solid border-gray0 rounded-xl w-fit h-fit mt-4 mb-1.5 px-6 py-3'>
-                      {
-                        v.profileUrl === null ?
-                          <Image src={basicProfile} width={24} height={24} />
-                          :
-                          <Image loader={() => v.profileUrl} src={v.profileUrl} width={24} height={24} />
-                      }
+                    <Box key={index} className='flex flex-row items-center border border-solid border-gray0 rounded-xl w-fit h-fit mt-4 px-6 py-3'>
+                      <Box className='border border-solid border-gray1 rounded-full px-1 py-0.5'>
+                        {
+                          v.time[5] === '0' ? (
+                            <Typography className='text-gray1 text-xs font-medium text-center min-w-[2rem]'>{v.time.slice(6, 7)}.{v.time.slice(8, 10)}</Typography>
+                          ) : (
+                            <Typography className='text-gray1 text-xs font-medium text-center min-w-[2rem]'>{v.time.slice(5, 7)}.{v.time.slice(8, 10)}</Typography>
+                          )
+                        }
+                      </Box>
+
+                      <Box className='ml-8'>
+                        {
+                          v.profileUrl === null ?
+                            <Image src={basicProfile} width={24} height={24} />
+                            :
+                            <Image loader={() => v.profileUrl} src={v.profileUrl} width={24} height={24} />
+                        }
+                      </Box>
+
                       <Typography className='text-black text-base font-medium ml-3 w-16'>{v.name}</Typography>
-                      <Typography className='text-gray1 text-xs font-regular ml-1'>· {v.age}</Typography>
+                      <Typography className='text-gray1 text-xs font-regular ml-2'>· {v.age}</Typography>
+                      <Typography className='text-black text-xs font-regular ml-5 w-1'>{v.mbti}</Typography>
                       <Button className='bg-white border border-solid border-accent1 rounded-full ml-32'>
                         <Typography className='text-accent1 text-xs font-regular'>프로필 보기</Typography>
                       </Button>
