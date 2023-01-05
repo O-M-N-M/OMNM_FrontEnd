fc1defe8927a6468e1ed8fd899ab526533bc58f4
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-05 14:28:00 +0900

    🔥 [DEL] : 사용하지 않는 코드 삭제
    사용하지 않는 코드를 삭제함

--- screen/mypage/mypage.screen.tsx
+++ screen/mypage/mypage.screen.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import { NextPage } from "next";
 import { getCookie } from "cookies-next";
