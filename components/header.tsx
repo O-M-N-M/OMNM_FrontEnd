3c0e49864f467ac54cbd76a610b3638fc47ca5c8
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-07 13:34:18 +0900

    🐛 [FIX] : 복구중 8
    복구중8

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getCookie, hasCookie } from "cookies-next";
+import { deleteCookie, getCookie, hasCookie, setCookie } from "cookies-next";
 
 import { Box, Typography } from "@mui/material";
 
@@ -8,41 +8,63 @@ import logo from '../public/logo2.png';
 import MyPageIcon from '../public/Group1.ico';
 
 import { useEffect, useState } from "react";
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const Header = () => {
-  const [cookie, setCookie] = useState(false);
-  const [profile, setProfile] = useState<string | null>(null)
+  const [isCookie, setIsCookie] = useState(false);
+  const [profile, setProfile] = useState<string | null>(null);
+
+  const checkRefreshToken = async () => {
+    const url = '/api/token';
+    const body = `accessToken=${getCookie('OMNM')}&refreshToken=${getCookie('refreshToken')}`;
+    await axios.post(url, body)
+      .then((res) => {
+        if (res.data === '재로그인 요청') {
+          alert('세션이 만료되었습니다.\n로그인을 다시해주세요.');
+          deleteCookie('OMNM');
+          document.location = '/login';
+        }
+        else {
+          setCookie('OMNM', res.data);
+
+          const router = useRouter();
+          router.replace(router.asPath);
+        }
+      });
+  };
 
   useEffect(() => {
     const loadProfile = async () => {
       const url = '/api/myInfo';
       const token = getCookie('OMNM');
-
-      const config: AxiosRequestConfig = {
-        method: 'get',
-        url: url,
+      const headers = {
         headers: {
           'OMNM': `${token}`
         }
-      }
+      };
 
-      const response: AxiosResponse = await axios(config);
-      setProfile(response.data.profileUrl);
+      axios.get(url, headers)
+        .then((res) => setProfile(res.data.profileUrl))
+        .catch((err) => {
+          if (err.response.status === 403) {
+            checkRefreshToken();
+          }
+        })
     };
 
     if (hasCookie('OMNM')) {
-      setCookie(true);
+      setIsCookie(true);
       loadProfile();
     } else {
-      setCookie(false);
+      setIsCookie(false);
     }
   })
 
   return (
     <Box className="flex items-center bg-white border-gray0 border-0 border-b border-solid w-auto h-[50px]">
       {
-        cookie ? (
+        isCookie ? (
           <>
             <Link href='/main'>
               <a className="ml-[15%]">
