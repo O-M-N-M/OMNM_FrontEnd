62476605efe9d402e3f046d5487661245d9240de
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-07 17:50:52 +0900

    ✨ [FEAT] : 회원가입 후 spash 추가
    회원가입 완료 후, 3초간 splash화면이 뜨고 success 페이지로 넘어가도록 함

--- screen/signup/signup_success.screen.tsx
+++ screen/signup/signup_success.screen.tsx
@@ -4,34 +4,58 @@ import { NextPage } from "next";
 import Image from "next/image";
 
 import signupSuccess from '../../public/signupSuccess.png';
+import splash from '../../public/splash.gif';
 import logo from '../../public/logo.png';
+import { useEffect, useState } from "react";
 
 export const SignUpSuccessScreen: NextPage = () => {
+  const [isSplash, setIsSplash] = useState<boolean>(true);
+
   const onClick = () => {
     document.location = '/login';
-  }
+  };
+
+  const showSplash = () => {
+    setTimeout(() => {
+      setIsSplash(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    showSplash();
+  })
 
   return (
-    <Box className='flex flex-col justify-center items-center w-full h-[calc(100vh-50px)]'>
-      <Image width={60} height={61} src={logo} />
+    <Box>
+      {
+        isSplash ? (
+          <Box className='bg-sky0 flex flex-col justify-center items-center w-full h-[calc(100vh-50px)]'>
+            <Image src={splash} />
+          </Box>
+        ) : (
+          <Box className='flex flex-col justify-center items-center w-full h-[calc(100vh-50px)]'>
+            <Image width={60} height={61} src={logo} />
 
-      <Box className="flex flex-col justify-center items-center border border-solid border-gray0 rounded-2xl h-max mt-5 px-96 py-14 text-center">
-        <Image width={81} height={75} src={signupSuccess} />
+            <Box className="flex flex-col justify-center items-center border border-solid border-gray0 rounded-2xl h-max mt-5 px-96 py-14 text-center">
+              <Image width={81} height={75} src={signupSuccess} />
 
-        <Box className="flex justify-center items-center mt-2">
-          <Typography className="text-4xl font-medium text-black">회원가입</Typography>
-          <Typography className="text-4xl font-regular text-black">이</Typography>
-          <Typography className="text-4xl font-medium text-black">&nbsp;완료</Typography>
-          <Typography className="text-4xl font-regular text-black">되었습니다.</Typography>
-        </Box>
+              <Box className="flex justify-center items-center mt-2">
+                <Typography className="text-4xl font-medium text-black">회원가입</Typography>
+                <Typography className="text-4xl font-regular text-black">이</Typography>
+                <Typography className="text-4xl font-medium text-black">&nbsp;완료</Typography>
+                <Typography className="text-4xl font-regular text-black">되었습니다.</Typography>
+              </Box>
 
-        <Box className="mt-4">
-          <Typography className="text-base text-black font-regular">회원가입을 축하합니다.</Typography>
-          <Typography className="text-base text-black font-regular">로그인한 후 룸메 찾기를 위한 설문조사를 진행해주세요.</Typography>
-        </Box>
+              <Box className="mt-4">
+                <Typography className="text-base text-black font-regular">회원가입을 축하합니다.</Typography>
+                <Typography className="text-base text-black font-regular">로그인한 후 룸메 찾기를 위한 설문조사를 진행해주세요.</Typography>
+              </Box>
 
-        <Button onClick={onClick} className="bg-accent1 rounded-full text-white text-sm font- block w-52 h-12 mt-11">로그인</Button>
-      </Box>
+              <Button onClick={onClick} className="bg-accent1 rounded-full text-white text-sm font- block w-52 h-12 mt-11">로그인</Button>
+            </Box>
+          </Box>
+        )
+      }
     </Box>
   );
 }
