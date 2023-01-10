6476eea033b0207f74b65c064a757c6e10732a43
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-10 12:01:02 +0900

    🔨 [MOD] : 푸터 링크 삭제 & 글자 크기 변경
    설문조사 링크 삭제하고,
    글자 크기를 변경함

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,36 +9,31 @@ const Footer = () => {
     <Box className="flex items-center bg-gray5 w-full h-[180px]">
       <Box className="ml-[15%]">
         <Image src={logo} width={60} height={61} />
-        <Typography className="text-gray4 text-xs font-regular mt-1">ⓒ 2023. omnm. All rights reserved.</Typography>
+        <Typography className="text-gray4 text-sm font-regular mt-1">ⓒ 2023. omnm. All rights reserved.</Typography>
       </Box>
 
       <Box className="flex flex-row ml-auto mr-[15%]">
         <Link href='/main'>
           <a>
-            <Typography className="font-medium text-xs">메인</Typography>
+            <Typography className="font-medium text-base">메인</Typography>
           </a>
         </Link>
 
         <Box className="ml-14">
           <Link href='/mypage'>
             <a>
-              <Typography className="font-medium text-xs mb-2">마이페이지</Typography>
+              <Typography className="font-medium text-base mb-2">마이페이지</Typography>
             </a>
           </Link>
 
-          <Link href='/mypage'>
-            <a>
-              <Typography className="font-regular text-xs mb-2">룸메 신청 내역</Typography>
-            </a>
-          </Link>
           <Link href='/mypage_surveyme'>
             <a>
-              <Typography className="font-regular text-xs mb-2">성향 설문조사</Typography>
+              <Typography className="font-regular text-base mb-2">성향 설문조사</Typography>
             </a>
           </Link>
           <Link href='/mypage_edit'>
             <a>
-              <Typography className="font-regular text-xs">설정</Typography>
+              <Typography className="font-regular text-base">설정</Typography>
             </a>
           </Link>
         </Box>
