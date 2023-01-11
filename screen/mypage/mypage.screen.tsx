7d2c8ba8f3b0a43830fdedeedd51f2f04542278c
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-11 22:48:06 +0900

    �ð [ADD] : 마이페이지 메인 리스트에 항목 없을 때 아이콘과 문구 추가함

--- screen/mypage/mypage.screen.tsx
+++ screen/mypage/mypage.screen.tsx
@@ -3,6 +3,7 @@ import { Box, CircularProgress, Typography } from "@mui/material";
 import { NextPage } from "next";
 import { getCookie } from "cookies-next";
 import Link from "next/link";
+import Image from 'next/image';
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -10,6 +11,7 @@ import axios from "axios";
 import Footer from '../../components/footer';
 import MyPageLeft from '../../components/mypage/mypage_left';
 import MyPageList from "../../components/mypage/mypage_list";
+import NoListIcon from '../../public/noListIcon.png';
 
 export const MyPageScreen: NextPage = () => {
   const [userId, setUserId] = useState('');
@@ -96,96 +98,115 @@ export const MyPageScreen: NextPage = () => {
           <MyPageLeft />
         </Box>
 
-        <Box className='border border-solid border-gray0 rounded-[1.25rem] w-full px-[2.875rem] py-16 ml-6'>
-          <Box>
-            <Box className='flex flex-row items-center mx-3.5'>
-              <Typography className='text-black text-xl font-medium'>룸메이트 신청 받은 리스트</Typography>
-              <Link href='/mypage_receivelist'>
-                <a className='ml-auto'>
-                  <Typography className='text-gray1 text-xs font-medium'>더보기</Typography>
-                </a>
-              </Link>
-            </Box>
-
-            {
-              (Array.isArray(receiveFirstData) && receiveFirstData.length === 0) ? (
-                <Box className='flex justify-center items-center w-full h-full mt-10'>
-                  <CircularProgress color="inherit" />
+        <Box className='border border-solid border-gray0 rounded-[1.25rem] w-full h-fit px-[2.875rem] py-16 ml-6'>
+          {
+            (Array.isArray(receiveFirstData) && receiveFirstData.length === 0) ? (
+              <Box className='flex justify-center items-center w-full h-full mt-10'>
+                <CircularProgress color="inherit" />
+              </Box>
+            ) :
+              (typeof receiveFirstData === 'undefined' && typeof receiveSecondData === 'undefined'
+                && typeof sendFirstData === 'undefined' && typeof sendSecondData === 'undefined') ? (
+                <Box className='flex flex-col justify-center items-center w-full h-[786px]'>
+                  <Image src={NoListIcon} width={44} height={55} />
+                  <Typography className='text-gray0 text-base font-regular mt-4'>신청을 주고 받은 룸메이트가 없습니다</Typography>
                 </Box>
               ) : (
-                <Box className='flex flex-wrap'>
-                  <Box className='w-[45%] mx-3.5'>
-                    <Typography className='text-gray1 text-xs font-regular mt-6'>{receiveFirstKey}</Typography>
+                <>
+                  <Box>
+                    <Box className='flex flex-row items-center mx-3.5'>
+                      <Typography className='text-black text-xl font-medium'>룸메이트 신청 받은 리스트</Typography>
+                      <Link href='/mypage_receivelist'>
+                        <a className='ml-auto'>
+                          <Typography className='text-gray1 text-xs font-medium'>더보기</Typography>
+                        </a>
+                      </Link>
+                    </Box>
+
                     {
-                      receiveFirstData && receiveFirstData.map((v: any, index: number) => {
-                        return (
-                          <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: true }} />
-                        )
-                      })
+                      (typeof receiveFirstData === 'undefined' && typeof receiveSecondData === 'undefined') ? (
+                        <Box className='flex flex-col justify-center items-center w-full mt-6'>
+                          <Image src={NoListIcon} width={44} height={55} />
+                          <Typography className='text-gray0 text-base font-regular mt-4'>신청 받은 룸메이트가 없습니다</Typography>
+                        </Box>
+                      ) : (
+                        <Box className='flex flex-wrap'>
+                          <Box className='w-[45%] mx-3.5'>
+                            <Typography className='text-gray1 text-xs font-regular mt-6'>{receiveFirstKey}</Typography>
+                            {
+                              receiveFirstData && receiveFirstData.map((v: any, index: number) => {
+                                return (
+                                  <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: true }} />
+                                )
+                              })
+                            }
+                          </Box>
+                          <Box className='w-[45%] mx-3.5'>
+                            <Typography className='text-gray1 text-xs font-regular mt-6'>{receiveSecondKey}</Typography>
+                            <>
+                              {
+                                receiveSecondData && receiveSecondData.map((v: any, index: number) => {
+                                  return (
+                                    <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: true }} />
+                                  )
+                                })
+                              }
+                            </>
+                          </Box>
+                        </Box>
+                      )
                     }
                   </Box>
-                  <Box className='w-[45%] mx-3.5'>
-                    <Typography className='text-gray1 text-xs font-regular mt-6'>{receiveSecondKey}</Typography>
-                    <>
-                      {
-                        receiveSecondData && receiveSecondData.map((v: any, index: number) => {
-                          return (
-                            <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: true }} />
-                          )
-                        })
-                      }
-                    </>
-                  </Box>
-                </Box>
-              )
-            }
-          </Box>
 
-          <Box className='mt-14'>
-            <Box className='flex flex-row items-center mx-3.5'>
-              <Typography className='text-black text-xl font-medium'>룸메이트 신청 보낸 리스트</Typography>
-              <Link href='/mypage_sendlist'>
-                <a className='ml-auto'>
-                  <Typography className='text-gray1 text-xs font-medium'>더보기</Typography>
-                </a>
-              </Link>
-            </Box>
+                  <Box className='mt-14'>
+                    <Box className='flex flex-row items-center mx-3.5'>
+                      <Typography className='text-black text-xl font-medium'>룸메이트 신청 보낸 리스트</Typography>
+                      <Link href='/mypage_sendlist'>
+                        <a className='ml-auto'>
+                          <Typography className='text-gray1 text-xs font-medium'>더보기</Typography>
+                        </a>
+                      </Link>
+                    </Box>
 
-            {
-              (Array.isArray(receiveSecondData) && receiveSecondData.length === 0) ? (
-                <Box className='flex justify-center items-center w-full h-full mt-10'>
-                  <CircularProgress color="inherit" />
-                </Box>
-              ) : (
-                <Box className='flex flex-wrap'>
-                  <Box className='w-[45%] mx-3.5'>
-                    <Typography className='text-gray1 text-xs font-regular mt-6'>{sendFirstKey}</Typography>
-                    <>
-                      {
-                        sendFirstData && sendFirstData.map((v: any, index: number) => {
-                          return (
-                            <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: false }} />
-                          )
-                        })
-                      }
-                    </>
+                    {
+                      (typeof sendFirstData === 'undefined' && typeof sendSecondData === 'undefined') ? (
+                        <Box className='flex flex-col justify-center items-center w-full mt-6'>
+                          <Image src={NoListIcon} width={44} height={55} />
+                          <Typography className='text-gray0 text-base font-regular mt-4'>신청 보낸 룸메이트가 없습니다</Typography>
+                        </Box>
+                      ) : (
+                        <Box className='flex flex-wrap'>
+                          <Box className='w-[45%] mx-3.5'>
+                            <Typography className='text-gray1 text-xs font-regular mt-6'>{sendFirstKey}</Typography>
+                            <>
+                              {
+                                sendFirstData && sendFirstData.map((v: any, index: number) => {
+                                  return (
+                                    <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: false }} />
+                                  )
+                                })
+                              }
+                            </>
+                          </Box>
+                          <Box className='w-[45%] mx-3.5'>
+                            <Typography className='text-gray1 text-xs font-regular mt-6'>{sendSecondKey}</Typography>
+                            <>
+                              {
+                                sendSecondData && sendSecondData.map((v: any, index: number) => {
+                                  return (
+                                    <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: false }} />
+                                  )
+                                })
+                              }
+                            </>
+                          </Box>
+                        </Box>
+                      )
+                    }
                   </Box>
-                  <Box className='w-[45%] mx-3.5'>
-                    <Typography className='text-gray1 text-xs font-regular mt-6'>{sendSecondKey}</Typography>
-                    <>
-                      {
-                        sendSecondData && sendSecondData.map((v: any, index: number) => {
-                          return (
-                            <MyPageList key={index} props={{ v: v, index: index, userName: userName, isReceive: false }} />
-                          )
-                        })
-                      }
-                    </>
-                  </Box>
-                </Box>
+                </>
               )
-            }
-          </Box>
+          }
         </Box>
       </Box>
 
