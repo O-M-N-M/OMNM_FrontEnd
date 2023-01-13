b344e03117d2c3a004a3fa9f9cc67ece5cc08a54
wisdomin121@naver.com <wisdomin121@naver.com>
2023-01-13 12:31:12 +0900

    ✨ [FEAT] : 리스트 없을 때 화면 생성

--- screen/mypage/mypage_receivelist.screen.tsx
+++ screen/mypage/mypage_receivelist.screen.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import { getCookie } from 'cookies-next';
+import Image from 'next/image';
 
 import { Box, IconButton, Pagination, PaginationItem, Typography } from "@mui/material";
 
@@ -7,21 +7,21 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import PrevButton from '../../public/prevButton.png';
+import DeleteIcon from '../../public/deleteIcon.png';
+import NoListIcon from '../../public/noListIcon.png';
+
 import Footer from "@/components/footer";
 import MyPageLeft from "@/components/mypage/mypage_left";
 import MyPageDetailList from '@/components/mypage/mypage_detaillist';
 
 export const MyPageReceiveListScreen = () => {
-  const [userId, setUserId] = useState('');
-  const [userName, setUserName] = useState('');
-
-  const [totalCount, setTotalCount] = useState<number>(0);
   const [data, setData] = useState<object[]>([]);
   const [index, setIndex] = useState<number>(0);
+  const [userId, setUserId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [totalCount, setTotalCount] = useState<number>(0);
 
-  const handleChange = (e: any, p: any) => {
-    setIndex(p - 1);
-  }
+  const handleChange = (_: any, p: any) => setIndex(p - 1);
 
   useEffect(() => {
     const token = getCookie('OMNM');
@@ -50,7 +50,10 @@ export const MyPageReceiveListScreen = () => {
 
       await axios.get(url, headers)
         .then((res) => {
-          setData(res.data);
+          if (res.data.length !== 0) {
+            setData(res.data);
+          }
+
           getTotalCount();
         })
         .catch((err) => console.log(err));
@@ -82,16 +85,28 @@ export const MyPageReceiveListScreen = () => {
               <Image src={PrevButton} width={18.44} height={23.48} />
             </IconButton>
             <Typography className='text-black text-xl font-medium ml-3'>룸메이트 신청 받은 리스트</Typography>
+            {
+              (totalCount > 0) && (
+                <IconButton onClick={() => document.location = '/mypage_deletereceivelist'} className='ml-auto'>
+                  <Image src={DeleteIcon} width={20} height={20} />
+                </IconButton>
+              )
+            }
           </Box>
 
           <Box className='flex flex-col items-center mt-5'>
             <Box className='w-full min-h-[44rem]'>
               {
-                data && data.map((v: any, index: number) => {
+                (totalCount > 0) ? data.map((v: any, index: number) => {
                   return (
                     <MyPageDetailList props={{ v: v, index: index, userName: userName, isReceive: true }} />
                   )
-                })
+                }) : (
+                  <Box className='flex flex-col justify-center items-center w-full h-[786px]'>
+                    <Image src={NoListIcon} width={44} height={55} />
+                    <Typography className='text-gray0 text-base font-regular mt-4'>신청 받은 룸메이트가 없습니다</Typography>
+                  </Box>
+                )
               }
             </Box>
 
--- screen/mypage/mypage_sendlist.screen.tsx
+++ screen/mypage/mypage_sendlist.screen.tsx
@@ -7,22 +7,21 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import PrevButton from '../../public/prevButton.png';
+import DeleteIcon from '../../public/deleteIcon.png';
+import NoListIcon from '../../public/noListIcon.png';
 
 import Footer from "@/components/footer";
 import MyPageLeft from "@/components/mypage/mypage_left";
 import MyPageDetailList from '@/components/mypage/mypage_detaillist';
 
 export const MyPageSendListScreen = () => {
-  const [userId, setUserId] = useState('');
-  const [userName, setUserName] = useState('');
-
-  const [totalCount, setTotalCount] = useState<number>(0);
   const [data, setData] = useState<object[]>([]);
   const [index, setIndex] = useState<number>(0);
+  const [userId, setUserId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [totalCount, setTotalCount] = useState<number>(0);
 
-  const handleChange = (e: any, p: any) => {
-    setIndex(p - 1);
-  }
+  const handleChange = (_: any, p: any) => setIndex(p - 1);
 
   useEffect(() => {
     const token = getCookie('OMNM');
@@ -51,7 +50,10 @@ export const MyPageSendListScreen = () => {
 
       await axios.get(url, headers)
         .then((res) => {
-          setData(res.data);
+          if (res.data.length !== 0) {
+            setData(res.data);
+          }
+
           getTotalCount();
         })
         .catch((err) => console.log(err));
@@ -83,16 +85,28 @@ export const MyPageSendListScreen = () => {
               <Image src={PrevButton} width={18.44} height={23.48} />
             </IconButton>
             <Typography className='text-black text-xl font-medium ml-3'>룸메이트 신청 보낸 리스트</Typography>
+            {
+              (totalCount > 0) && (
+                <IconButton onClick={() => document.location = '/mypage_deletesendlist'} className='ml-auto'>
+                  <Image src={DeleteIcon} width={20} height={20} />
+                </IconButton>
+              )
+            }
           </Box>
 
           <Box className='flex flex-col items-center mt-9'>
             <Box className='w-full min-h-[44rem]'>
               {
-                data && data.map((v: any, index: number) => {
+                (totalCount > 0) ? data.map((v: any, index: number) => {
                   return (
                     <MyPageDetailList key={index} props={{ v: v, index: index, userName: userName, isReceive: false }} />
                   )
-                })
+                }) : (
+                  <Box className='flex flex-col justify-center items-center w-full h-[786px]'>
+                    <Image src={NoListIcon} width={44} height={55} />
+                    <Typography className='text-gray0 text-base font-regular mt-4'>신청 보낸 룸메이트가 없습니다</Typography>
+                  </Box>
+                )
               }
             </Box>
 
